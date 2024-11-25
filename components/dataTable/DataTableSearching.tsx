03371cba83978ff60c887860e3c877c260c3fffb
vitalik <burbanvitalik2002@gmail.com>
2024-11-25 19:30:34 +0200

    add useDebounce hook

--- components/dataTable/DataTableSearching.tsx
+++ components/dataTable/DataTableSearching.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { X } from "lucide-react";
 import { Table as ReactTable } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/app/hooks";
 
 type PropsType<TData> = {
   dataTable: ReactTable<TData>;
@@ -12,31 +16,41 @@ export const DataTableSearching = <TData extends object>({
   dataTable,
   filterByColumn,
 }: PropsType<TData>) => {
-  // Remove input value
-  const handleClearFilter = () => {
-    dataTable.getColumn(filterByColumn)?.setFilterValue("");
+  const [filterValue, setFilterValue] = useState<string>(
+    (dataTable.getColumn(filterByColumn)?.getFilterValue() as string) ?? ""
+  );
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(event.target.value);
   };
 
-  // Change input value
-  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) =>
-    dataTable.getColumn(filterByColumn)?.setFilterValue(event.target.value);
+  const handleClearFilter = () => {
+    setFilterValue("");
+  };
 
-  // Filtered value
-  const filteredValue =
-    (dataTable.getColumn(filterByColumn)?.getFilterValue() as string) ?? "";
+  const debouncedFilterValue = useDebounce(filterValue, 1000);
+
+  useEffect(() => {
+    if (
+      debouncedFilterValue !==
+      (dataTable.getColumn(filterByColumn)?.getFilterValue() as string)
+    ) {
+      dataTable.getColumn(filterByColumn)?.setFilterValue(debouncedFilterValue);
+    }
+  }, [debouncedFilterValue, dataTable, filterByColumn]);
 
   return (
     <div className="relative w-full sm:max-w-xs md:max-w-md lg:max-w-sm">
       <Input
         placeholder={"Search..."}
-        value={filteredValue}
+        value={filterValue}
         onChange={handleChangeValue}
         className="pr-12 py-2 border"
       />
       <Button
         onClick={handleClearFilter}
         className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 bg-transparent text-gray-500 focus:outline-none"
-        disabled={!filteredValue}
+        disabled={!filterValue}
         variant="ghost"
       >
         <X />
