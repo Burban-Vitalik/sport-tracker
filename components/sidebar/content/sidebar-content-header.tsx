87cbdae887fab075ad1de51dd4a26e9334f629e9
vitalik <burbanvitalik2002@gmail.com>
2025-01-26 19:33:07 +0200

    Refactor: remove url for nav header

--- components/sidebar/content/sidebar-content-header.tsx
+++ components/sidebar/content/sidebar-content-header.tsx
@@ -1,33 +1,9 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "../../ui/breadcrumb";
 import { ToggleSidebarButton } from "./toggle-sidebar";
 
-type PropsType = {
-  data?: number;
-};
-
-export const SidebarContentHeader = ({}: PropsType) => {
+export const SidebarContentHeader = () => {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
       <ToggleSidebarButton />
-
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem className="hidden md:block">
-            <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator className="hidden md:block" />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
     </header>
   );
 };
